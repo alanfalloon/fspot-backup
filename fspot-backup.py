ec45475b9f6265e93bee88ae82f8f80d0bf583ce
Alan Falloon <alan.falloon@gmail.com>
2011-01-27 22:57:11 -0500

    Name the files based on the date they were taken
    Instead of naming the files numerically based on their order in the
    database, separate them into directories by year, month and day, then
    name them based on the time they were taken. Only fall back to the
    numeric naming scheme if the date information is missing from the
    file.

--- fspot-backup.py
+++ fspot-backup.py
@@ -5,6 +5,28 @@
 
 import sys, os
 import urllib
+import EXIF
+
+photonum=0
+def get_new_image_name(img,ver):
+    global photonum
+    f = open(img, 'rb')
+    try:
+        tags = EXIF.process_file(f, strict=True, stop_tag="DateTimeOriginal")
+        if "EXIF DateTimeOriginal" not in tags:
+            photonum += 1
+            return "photo%04d" % (photonum,)
+        d = tags["EXIF DateTimeOriginal"]
+        (day,time) = str(d).split()
+        (y,m,d) = day.split(":")
+        (H,M,S) = time.split(":")
+        v=""
+        if ver > 1:
+            v="_v%d"%(ver,)
+        return "%s/%s/%s/%s-%s-%s%s" % (y,m,d,H,M,S,v)
+        
+    finally:
+        f.close()
 
 try:
     dest_path = sys.argv[1]
@@ -28,21 +50,28 @@ db = sqlite3.connect('fspot.db')
 c = db.cursor()
 c.execute('''delete from "exports"''')
 
-photonum=0
-def backup_photo(uri):
+def backup_photo(uri,ver=1):
     "Backup the photo, return the new uri"
-    global photonum
     if not uri.startswith("file://"): raise ValueError, uri
     oldname=urllib.unquote(uri[7:])
     suffix=uri[uri.rfind('.'):]
-    newname="photo%04d%s" % (photonum,suffix)
-    photonum+=1
-    if os.spawnlp(os.P_WAIT, 'cp', 'cp', '-l', oldname, newname): raise ValueError, (oldname,newname)
-    return ('file://',newname)
+    newname=get_new_image_name(oldname,ver)+suffix
+    (dirname,filename) = os.path.split(newname)
+    if dirname:
+        dirname = dirname+"/"
+        if os.spawnlp(os.P_WAIT, 'mkdir', 'mkdir', '-p', dirname): raise ValueError, dirname
+    basename=os.path.splitext(filename)[0]
+    uniquename=basename
+    x=1
+    while os.spawnlp(os.P_WAIT, 'cp', 'cp', '-l', oldname,
+                     os.path.join(dirname,uniquename+suffix)):
+        uniquename="%s_%d" % (basename,x)
+        x += 1
+    return ('file://'+dirname,uniquename+suffix)
 c.execute('''select id, base_uri, filename from photos''')
 new_photos = [(id, backup_photo(uri+fn)) for id,uri,fn in c]
 c.execute('''select photo_id, version_id, base_uri, filename from photo_versions where version_id <> 1''')
-new_versions = [(pid, vid, backup_photo(uri+fn)) for pid,vid,uri,fn in c]
+new_versions = [(pid, vid, backup_photo(uri+fn,vid)) for pid,vid,uri,fn in c]
 for id, (uri, fn) in new_photos:
     c.execute("update photos set base_uri=?, filename=? where id=?",(uri,fn,id))
 for id, (uri, fn) in new_photos:
